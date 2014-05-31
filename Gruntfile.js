9a77b9e8f5ed8da070954dbabe16f593cbda9a89
taqun <envoixnet@gmail.com>
2014-06-01 04:05:50 +0900

    Add gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'src',
+          livereload: true
+        }
+      }
+    },
+
+    watch: {
+      livereload: {
+        files: [
+          'src/*.html'
+        ],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['connect','watch']);
+};
